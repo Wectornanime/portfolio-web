ea4000e489ee11da55af1813a319ab39be468707
Wectornanime <wectornanime@gmail.com>
2026-02-24 08:28:27 -0300

    refactor: aplication layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@heroui/link";
+import { Divider } from "@heroui/divider";
 
 import { Navbar } from "@/components/navbar";
 
@@ -10,17 +11,27 @@ export default function DefaultLayout({
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container mx-auto max-w-7xl px-6 flex-grow">
         {children}
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
+      <footer className="w-full flex items-center justify-center py-3 gap-2">
+        <Link
+          isExternal
+          className="flex items-center gap-1 text-current"
+          href="https://github.com/Wectornanime"
+          title="Wectornanime's github"
+        >
+          <span className="text-default-600">Powered by</span>
+          <p className="text-primary">Wectornanime</p>
+        </Link>
+        <Divider orientation="vertical" />
         <Link
           isExternal
           className="flex items-center gap-1 text-current"
           href="https://heroui.com"
           title="heroui.com homepage"
         >
-          <span className="text-default-600">Powered by</span>
+          <span className="text-default-600">Using</span>
           <p className="text-primary">HeroUI</p>
         </Link>
       </footer>

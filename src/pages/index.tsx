ad1eeace47914c7ff590c1e2b2e96acf03f555c7
Wectornanime <wectornanime@gmail.com>
2026-02-25 19:05:57 -0300

    refactor: move the index page to a new structure

--- src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function IndexPage() {
-  return <div className="bg-red-500 flex w-full h-full" />;
-}
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,3 @@
+export default function IndexPage() {
+  return <div className="bg-red-500 h-full w-full" />;
+}

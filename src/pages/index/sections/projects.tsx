8f22f97ddbfd1e65c29b1945ee46157ad7f4f263
Wectornanime <wectornanime@gmail.com>
2026-03-16 19:45:54 -0300

    refactor: add dynamic content into project section

--- src/pages/index/sections/projects.tsx
+++ src/pages/index/sections/projects.tsx
@@ -2,7 +2,13 @@ import { Button, Image, ScrollShadow } from "@heroui/react";
 
 import { Section } from "../components/section";
 
-export default function ProjectsSection() {
+import { ProjectApiResponseType } from "@/types/api";
+
+interface ProjectsSectionProps {
+  content: ProjectApiResponseType[];
+}
+
+export default function ProjectsSection({ content }: ProjectsSectionProps) {
   return (
     <Section>
       <h3 className="text-xl mb-3 pl-6">Projetos</h3>
@@ -11,46 +17,42 @@ export default function ProjectsSection() {
         className="flex gap-2 w-full"
         orientation="horizontal"
       >
-        {[...Array(20)].map((_, index) => (
-          <div key={index} className="fit rounded-md bg-default">
+        {content.map((project) => (
+          <div key={project.id} className="fit rounded-md bg-default">
             <div>
               <Image
-                alt="Woman listing to music"
+                alt={project.title}
                 className="object-cover rounded-t-sm"
                 height={120}
                 radius="none"
-                src="https://heroui.com/images/hero-card.jpeg"
+                src={project.imageUrl}
                 width={260}
               />
             </div>
             <div className="px-2">
-              <h4 className="font-bold text-large">Projeto 1</h4>
+              <h4 className="font-bold text-large line-clamp-1">
+                {project.title}
+              </h4>
               <p className="text-tiny text-wrap h-[100px] w-[220px] line-clamp-6">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Mollitia illum eligendi aut! Explicabo illum fuga excepturi ab
-                possimus, quas accusantium quam, nam perspiciatis itaque culpa
-                voluptatibus, tempora dolorem facilis accusamus.
+                {project.text}
               </p>
             </div>
             <div className="flex justify-around py-2">
-              <Button
-                className="text-tiny tracking-wide uppercase text-white bg-black/40"
-                color="default"
-                radius="sm"
-                size="sm"
-                variant="flat"
-              >
-                Github
-              </Button>
-              <Button
-                className="text-tiny tracking-wide uppercase text-white bg-black/40"
-                color="default"
-                radius="sm"
-                size="sm"
-                variant="flat"
-              >
-                site
-              </Button>
+              {project.links.map((link) => (
+                <Button
+                  key={link.id}
+                  as={"a"}
+                  className="text-tiny tracking-wide uppercase text-white bg-black/40"
+                  color="default"
+                  href={link.link}
+                  radius="sm"
+                  size="sm"
+                  target="_blank"
+                  variant="flat"
+                >
+                  {link.title}
+                </Button>
+              ))}
             </div>
           </div>
         ))}

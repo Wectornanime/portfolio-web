94d5ce977945200b3248a024095663934d683fc5
Wectornanime <wectornanime@gmail.com>
2026-03-16 19:34:53 -0300

    feat: create the loading overlay component

---
+++ src/components/loading.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from "@heroui/react";
+
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <Spinner color="white" size="lg" />
+    </div>
+  );
+}

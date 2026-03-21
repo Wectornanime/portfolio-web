7d7e5508356cc441a21ae32740c27e8a2f5708ba
Wectornanime <wectornanime@gmail.com>
2026-03-21 19:19:49 -0300

    refactor: adds api.ts to create api instance

--- src/services/getUserInfo.service.ts
+++ src/services/getUserInfo.service.ts
@@ -1,11 +1,10 @@
-import axios from "axios";
-
+import { api } from "@/lib/api";
 import { ApiResponse } from "@/types/api";
 
 export default async function getUserInfo(): Promise<ApiResponse | undefined> {
   try {
-    const { data, status } = await axios.get<{ data: ApiResponse }>(
-      "http://localhost:3000/api/public/wectornanime",
+    const { data, status } = await api.get<{ data: ApiResponse }>(
+      "wectornanime",
     );
 
     if (status === 200) {
